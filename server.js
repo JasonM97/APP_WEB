275bbc139e25bdc10e5c5453ba590cbe5f56febe
JasonM97 <jason97gts@gmail.com>
2024-08-18 21:18:55 -0500

    PAGINA CONTACTO

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 // const db = mysql.createConnection({
 //     host: 'localhost',
 //     user: 'root',
-//     password: 'Tsuki1904',
+//     password: 'root',
 //     database: 'formulario_contacto'
 // });
 
